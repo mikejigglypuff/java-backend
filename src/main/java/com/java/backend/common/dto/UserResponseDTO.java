908c8f4b2f45e8f941c4f23c094f27674b5b8d77
mikejigglypuff <50828241+mikejigglypuff@users.noreply.github.com>
2025-04-19 18:43:55 +0900

    fix: fix UserResponseDTO

--- src/main/java/com/java/backend/common/dto/UserResponseDTO.java
+++ src/main/java/com/java/backend/common/dto/UserResponseDTO.java
@@ -3,10 +3,11 @@ package com.java.backend.common.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UserResponseDTO(
-        @Schema(description = "회원 이메일", example = "example@mail.com")
-        String email,
         @Schema(description = "회원 닉네임", example = "example")
-        String nickname
+        String nickname,
+
+        @Schema(description = "회원 이메일", example = "example@mail.com")
+        String email
 ) {
 
 }
